b68855daacbda24be20d7e7e518baea7b75fd301
Iwasawa <t_iwasawa1@hotmail.com>
2025-12-05 23:03:29 +0900

    問題5

--- constants.py
+++ constants.py
@@ -51,7 +51,7 @@ SUPPORTED_EXTENSIONS = {
     ".pdf": PyMuPDFLoader,
     ".docx": Docx2txtLoader,
     ".csv": lambda path: CSVLoader(path, encoding="utf-8"),
-    ".txt": lambda path: TextLoader(path, encoding="utf-8")
+    ".txt": lambda path: TextLoader(path, encoding="utf-8", autodetect_encoding=True)
 }
 WEB_URL_LOAD_TARGETS = [
     "https://generative-ai.web-camp.io/"
